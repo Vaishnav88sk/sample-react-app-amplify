708113193dd6e52a593deecf5ede6e24a3ff0837
Vaishnav Kale <vaishnavsk8804@gmail.com>
2024-08-23 18:34:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <footer>
+          <p>&copy; 2024 My Sample Website</p>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
